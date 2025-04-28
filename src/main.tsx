78c08149fcddd1be864c7506ddb1347192d73bee
nexus <grupoeducacionalnexus>
2025-04-28 12:59:32 -0300

    up

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="REACT_APP_CLIENT_ID_GOOGLE">
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
